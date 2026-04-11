0bf4f0901b25b14a0164adebd728f4101e5620bf
sweekyl <kirilllitv42@gmail.com>
2026-04-11 20:20:40 +0500

    Отключил скилы.лаваленд.прибытие.

--- Content.Shared/_CorvaxGoob/CCCVars/CCCVars.cs
+++ Content.Shared/_CorvaxGoob/CCCVars/CCCVars.cs
@@ -82,7 +82,7 @@ public sealed class CCCVars
     /// Will skills be applied to players.
     /// </summary>
     public static readonly CVarDef<bool> SkillsEnabled =
-        CVarDef.Create("skills.enabled", true, CVar.SERVERONLY);
+        CVarDef.Create("skills.enabled", false, CVar.SERVERONLY);
 
     /// <summary>
     /// Show MRP-designated jobs in the lobby and late join.
