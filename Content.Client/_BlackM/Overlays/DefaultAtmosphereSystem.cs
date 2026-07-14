5af6d5a031a9622f0c96c595866d29bbc5087889
sweekyl <kirilllitv42@gmail.com>
2026-07-14 09:29:07 +0500

    шейдер теперь можно выключить в настройках

--- Content.Client/_BlackM/Overlays/DefaultAtmosphereSystem.cs
+++ Content.Client/_BlackM/Overlays/DefaultAtmosphereSystem.cs
@@ -1,5 +1,8 @@
+using Content.Shared._BlackM.CCVar;
 using Robust.Client.Graphics;
+using Robust.Shared.Configuration;
 using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
 
 namespace Content.Client._BlackM.Overlays;
 
@@ -9,16 +12,48 @@ namespace Content.Client._BlackM.Overlays;
 public sealed class DefaultAtmosphereSystem : EntitySystem
 {
     [Dependency] private readonly IOverlayManager _overlay = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    private DefaultOverlay? _overlayInstance;
 
     public override void Initialize()
     {
         base.Initialize();
-        _overlay.AddOverlay(new DefaultOverlay());
+
+        _cfg.OnValueChanged(BlackMCVars.DefaultAtmosphereShaderEnabled, OnShaderToggled, invokeImmediately: true);
     }
 
     public override void Shutdown()
     {
         base.Shutdown();
-        _overlay.RemoveOverlay<DefaultOverlay>();
+
+        _cfg.UnsubValueChanged(BlackMCVars.DefaultAtmosphereShaderEnabled, OnShaderToggled);
+        RemoveOverlay();
+    }
+
+    private void OnShaderToggled(bool enabled)
+    {
+        if (enabled)
+            AddOverlay();
+        else
+            RemoveOverlay();
+    }
+
+    private void AddOverlay()
+    {
+        if (_overlayInstance != null)
+            return;
+
+        _overlayInstance = new DefaultOverlay();
+        _overlay.AddOverlay(_overlayInstance);
+    }
+
+    private void RemoveOverlay()
+    {
+        if (_overlayInstance == null)
+            return;
+
+        _overlay.RemoveOverlay(_overlayInstance);
+        _overlayInstance = null;
     }
 }
---
+++ Resources/Locale/ru-RU/_BlackM/options/options.ftl
@@ -0,0 +1,3 @@
+ui-options-default-atmosphere-shader = Шейдер (Повышенная гамма).
+ui-options-echo-enabled = Эффект эха
+ui-options-echo-strong-preset = Усиленное эхо
