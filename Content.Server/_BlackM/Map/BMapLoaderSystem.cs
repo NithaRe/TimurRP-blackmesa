7261033191bb4c27f6aae781d4239f1a8b992815
sweekyl <kirilllitv42@gmail.com>
2026-09-05 05:15:26 +0500

    фикс зен карты проблема с консолью

--- Content.Server/_BlackM/Map/BMapLoaderSystem.cs
+++ Content.Server/_BlackM/Map/BMapLoaderSystem.cs
@@ -1,6 +1,10 @@
+using System.Linq;
 using Content.Server.GameTicking.Events;
+using Content.Server.Maps;
+using Content.Server.Station.Systems;
 using Robust.Shared.EntitySerialization;
 using Robust.Shared.EntitySerialization.Systems;
+using Robust.Shared.Prototypes;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Utility;
@@ -10,6 +14,8 @@ namespace Content.Server._BlackM.Map;
 public sealed class BMapLoaderSystem : EntitySystem
 {
     [Dependency] private readonly MapLoaderSystem _mapLoader = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly StationSystem _station = default!;
 
     public override void Initialize()
     {
@@ -26,12 +32,28 @@ public sealed class BMapLoaderSystem : EntitySystem
 
     private void LoadMap(string path)
     {
-        var options = new DeserializationOptions
+        if (path == "/Maps/_BlackM/ZenMap/zen.yml")
+        {
+            if (!_mapLoader.TryLoadMap(new ResPath(path), out _, out var grids, new DeserializationOptions
+            {
+                InitializeMaps = true,
+                PauseMaps = false,
+            }))
+            {
+                return;
+            }
+
+            var prototype = _prototypeManager.Index<GameMapPrototype>("ZenBlackM");
+            var config = prototype.Stations["ZenStation"];
+            _station.InitializeNewStation(config, grids.Select(grid => grid.Owner));
+
+            return;
+        }
+
+        _mapLoader.TryLoadMap(new ResPath(path), out _, out _, new DeserializationOptions
         {
             InitializeMaps = true,
             PauseMaps = false,
-        };
-
-        _mapLoader.TryLoadMap(new ResPath(path), out _, out _, options);
+        });
     }
 }
